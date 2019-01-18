aee1d5ec8c0eff1263cad452e6a78eab3db4d2cf
Holubinka Vitalik <holybinka-1996@outlook.com>
2019-01-18 13:30:11 +0200

    My Homework 1_1

---
+++ src/com/company/Homework1/Homework1_1.java
@@ -0,0 +1,32 @@
+package com.company.Homework1;
+
+import java.util.Arrays;
+
+public class Homework1_1 {
+
+    public static void main(String[] args) {
+        int[] arr = {5, 6, 1, 2, 6, 7, 9};
+        int position = 5;
+
+        rotated(arr, position);
+    }
+
+    private static  void rotated (int[] arr, int position){
+        int [] temp = Arrays.copyOf(arr, arr.length);
+
+//        for(int i = 0; i < position; i++) {
+//            arr[arr.length - position + i] = temp[i];
+//        }
+        for (int i = 0; i < temp.length; i++){
+            if(i < position){
+                arr [arr.length - position + i] = temp[i];
+            } else {
+                arr [i - position] = temp[i];
+            }
+        }
+
+        for (Integer list : arr) {
+            System.out.print(list + " ");
+        }
+    }
+}
