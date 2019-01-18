13c27cbce215be6c305a35547a0950ae04905579
Holubinka Vitalik <holybinka-1996@outlook.com>
2019-01-18 13:55:36 +0200

    My Homework 1_2

--- src/com/company/Homework1/Homework1_2.java
+++ src/com/company/Homework1/Homework1_2.java
@@ -4,19 +4,16 @@ import java.util.Arrays;
 
 public class Homework1_2 {
     public  static void main(String[] args) {
-        int[] arr = {1, 2, -1, 4, 7, 6, -2, 4};
+        int[] arr = {1, 2, -1, 4, 7, 6, -2, 4, 3, -5, 3, 4 };
         restore(arr);
     }
 
     private  static  void restore(int[] arr){
         int [] temp = Arrays.copyOf(arr, arr.length);
 
-        for (int i = 2; i < temp.length; i++){
-            if(temp[i] - temp[i - 2] == 2 && temp[i - 1] < 0 ){
-                temp[i - 1] = temp[i] - 1;
-            }
-            if(temp[i] - temp[i - 2] == -2 && temp[i - 1] < 0 ){
-                temp[i - 1] = temp[i] + 1;
+        for (int i = 1; i < temp.length-1; i++){
+            if(Math.abs(temp[i + 1] - temp[i - 1]) == 2 && temp[i] < 0 ){
+                temp[i] = (temp[i + 1] + temp[i - 1]) / 2;
             }
         }
         for (Integer list : temp) {
