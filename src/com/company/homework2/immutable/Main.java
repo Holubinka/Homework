64efa8adc3bc25595e3dce519cf9a6897a9de710
Holubinka Vitalik <holybinka-1996@outlook.com>
2019-01-23 10:39:12 +0200

    Edit homework

--- src/com/company/homework2/immutable/Main.java
+++ src/com/company/homework2/immutable/Main.java
@@ -1,43 +1,6 @@
 package com.company.homework2.immutable;
 
 import java.util.Date;
-final class ImmutablePerson {
-    private final String name;
-    private final int age;
-    private final String colorEye;
-    private final Date birthDay;
-
-    public ImmutablePerson(String name, int age, String colorEye, Date birthDay) {
-        this.name = name;
-        this.age = age;
-        this.colorEye = colorEye;
-        this.birthDay = new Date(birthDay.getTime());
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public String getColorEye() {
-        return colorEye;
-    }
-
-    public Date getBirthDay() {
-        return new Date(birthDay.getTime());
-    }
-
-    @Override
-    public String toString() {
-        return "name='" + name + '\'' +
-                ", age=" + age +
-                ", colorEye='" + colorEye + '\'' +
-                ", birthDay=" + birthDay;
-    }
-}
 
 public class Main {
     public static void main(String[] args) {
