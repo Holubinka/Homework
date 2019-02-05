9803cae36f12c14886d15ea304de6a5cd4f281ef
Holubinka Vitalik <holybinka-1996@outlook.com>
2019-02-05 11:13:58 +0200

    Edit mistakes

--- src/com/company/homework4/ArrayList.java
+++ src/com/company/homework4/ArrayList.java
@@ -3,28 +3,25 @@ package com.company.homework4;
 import java.util.Arrays;
 
 public class ArrayList<T> implements List<T> {
-    private final int INIT_CAPACITY;
-    private final int DEFAULT_CAPACITY = 10;
-    private final double COEFFICIENT = 1.5;
-    private T[] array;
+    private static final int DEFAULT_CAPACITY = 10;
+    private static final double COEFFICIENT = 1.5;
+    private Object[] data;
     private int size = 0;
 
     public ArrayList() {
-        this.INIT_CAPACITY = 10;
-        array = (T[]) new Object[DEFAULT_CAPACITY];
+        data = new Object[DEFAULT_CAPACITY];
     }
 
-    public ArrayList(int INIT_CAPACITY) {
-        this.INIT_CAPACITY = INIT_CAPACITY;
-        array = (T[]) new Object[INIT_CAPACITY];
+    public ArrayList(int initialCapacity) {
+        data = new Object[initialCapacity];
     }
 
     @Override
     public void add(T t) {
-        if (size == array.length) {
-            resize((int) (array.length * COEFFICIENT) + 1);
+        if (size == data.length) {
+            resize((int) (data.length * COEFFICIENT) + 1);
         } else {
-            array[size++] = t;
+            data[size++] = t;
         }
     }
 
@@ -36,9 +33,9 @@ public class ArrayList<T> implements List<T> {
     @Override
     public T get(int index) {
         if (index < size) {
-            return array[index];
+            return (T) data[index];
         } else {
-            throw new ArrayIndexOutOfBoundsException("Некоректний індекс");
+            throw new ArrayIndexOutOfBoundsException("Index: " + index + ", Size: " + size);
         }
     }
 
@@ -46,27 +43,26 @@ public class ArrayList<T> implements List<T> {
     public void remove(int index) {
         if (index < size) {
             int numMoved = size - index - 1;
-            System.arraycopy(array, index + 1, array, index, numMoved);
+            System.arraycopy(data, index + 1, data, index, numMoved);
 
-            array[--size] = null;
+            data[--size] = null;
 
-            if (array.length > DEFAULT_CAPACITY && size < array.length / COEFFICIENT)
-                resize((int) ((array.length - 1) / COEFFICIENT));
+            if (data.length > DEFAULT_CAPACITY && size < data.length / COEFFICIENT)
+                resize((int) ((data.length - 1) / COEFFICIENT));
         } else {
-            throw new ArrayIndexOutOfBoundsException("Некоректний індекс");
+            throw new ArrayIndexOutOfBoundsException("Index: " + index + ", Size: " + size);
         }
     }
 
-    private T[] resize(int newCapacity) {
-        T[] temp = Arrays.copyOf(array, newCapacity);
-        return temp;
+    private void resize(int newCapacity) {
+        data = Arrays.copyOf(data, newCapacity);
     }
 
     @Override
     public String toString() {
-        T[] temp = (T[])new Object[size];
+        T[] temp = (T[]) new Object[size];
         for (int i = 0; i < temp.length; i++) {
-            temp[i] = array[i];
+            temp[i] = (T) data[i];
         }
         return Arrays.toString(temp);
     }
